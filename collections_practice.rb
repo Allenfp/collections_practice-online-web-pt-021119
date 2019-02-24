bd279624d8843e01fece29cf109d1705149c4a65
Allen F Partlow <allenfp@gmail.com>
2019-02-24 05:41:24 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -16,6 +16,16 @@ end
 
 def sort_array_desc
 
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif a < b
+      1
+    elsif a > b
+      -1
+    end
+  end
+
 end
 
 def sort_array_char_count
