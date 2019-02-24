15b1a8efcedada62b90751eb41f5d02359da33ca
Allen F Partlow <allenfp@gmail.com>
2019-02-24 05:44:54 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -31,18 +31,14 @@ end
 def sort_array_char_count(array)
 
   array.sort do |a, b|
-    if a.length == b.length
-      0
-    elsif a.length > b.length
-      1
-    elsif a.length < b.length
-      -1
+    if a.length <=> b.length
+
     end
   end
 
 end
 
-def swap_elements
+def swap_elements(array)
 
 end
 
